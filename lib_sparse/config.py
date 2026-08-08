ff51a5a94b436b04aead8201a29c44fd28b6eb59
Max Zhu <mz406@cam.ac.uk>
2026-08-08 20:35:35 +0100

    Split b15

--- lib_sparse/config.py
+++ lib_sparse/config.py
@@ -5,5 +5,5 @@ RELU2_SCALE = 1
 BLOCK_M = 64        # Rows per tile
 BLOCK_N = 64        # Columns per tile
 
-# Number of tiles used for temporary 16-bit staging while packing the final 15-bit stream.
+# Number of tiles per chunk used for temporary 16-bit staging while packing the final 15-bit stream.
 _PACK_15BIT_CHUNK_TILES = 2**16
